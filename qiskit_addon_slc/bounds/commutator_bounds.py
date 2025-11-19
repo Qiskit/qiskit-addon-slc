1ee90a4bed00d08d4e19b70709711c359c0ee774
Max Rossmannek <oss@zurich.ibm.com>
2025-11-19 15:30:17 +0100

    Apply suggestions from code review
    Co-authored-by: aeddins-ibm <60495383+aeddins-ibm@users.noreply.github.com>

--- qiskit_addon_slc/bounds/commutator_bounds.py
+++ qiskit_addon_slc/bounds/commutator_bounds.py
@@ -215,10 +215,10 @@ def compute_bounds(
         # PERF: using map_async would be more performant than apply_async for each term
         # individually. But that would imply we are still forced to process all layers as a whole
         # and cannot process individual terms.
-        for pauli_idx, pauli in enumerate(local_noise_terms):
+        for pauli_idx, pauli in enumerate(local_noise_terms.to_pauli_list()):
             task = pool.apply_async(
                 norm_fn,
-                [pauli.to_pauli()],
+                [pauli],
                 callback=partial(_insert_rate, box_id=box_id, rate_idx=pauli_idx),
             )
             tasks.append(task)
