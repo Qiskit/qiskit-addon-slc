2c030a0ef2a6da756ec486a23a33b377d85d5993
Caleb Johnson <calebj1524@outlook.com>
2026-04-22 01:28:36 -0500

    Use machine precision as tolerance during propagation (#60)
    * Use machine precision as tolerance during backprop
    
    * style
    
    * feat: introduce qiskit_addon_slc.globals.ZERO_ATOL
    
    ---------
    
    Co-authored-by: Max Rossmannek <oss@zurich.ibm.com>

---
+++ docs/apidocs/qiskit_addon_slc.globals.rst
@@ -0,0 +1,8 @@
+=========================================
+Globals (:mod:`qiskit_addon_slc.globals`)
+=========================================
+
+.. automodule:: qiskit_addon_slc.globals
+   :no-members:
+   :no-inherited-members:
+   :no-special-members:
--- qiskit_addon_slc/bounds/backward.py
+++ qiskit_addon_slc/bounds/backward.py
@@ -33,6 +33,7 @@ from qiskit.quantum_info import (
     SparsePauliOp,
 )
 
+from ..globals import ZERO_ATOL
 from ..utils import remove_measure
 from .commutator_bounds import Bounds, CommutatorBounds, compute_bounds
 from .light_cone import LightCone
@@ -78,7 +79,7 @@ def _time_evolved_norm_backward(
         operator=pauli,
         rot_gates=gates,
         max_terms=evolution_max_terms,
-        atol=0,
+        atol=ZERO_ATOL,
         frame="s",
     )
     trunc_bias = 2 * trunc_onenorm
